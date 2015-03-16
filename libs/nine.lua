3e438caf0d0c7d29a0889f183c9ea74a2ae3d09c
Pablo Mayobre <pablo.a.mayobre@gmail.com>
2015-03-16 14:39:03 -0300

    Changes to nine.lua
    Added some meta
    Finished nine.convert (not tested)
    PS: This does not fix the broken lines issue, that will be hard

--- libs/nine.lua
+++ libs/nine.lua
@@ -1,3 +1,32 @@
+local nine = {
+	_VERSION     = 'nine.lua v0.1.0',
+	_DESCRIPTION = 'Nine Patch implementation for LOVE (Part of Material-Love)',
+	_URL         = 'https://www.github.com/Positive07/material-love/tree/master/libs/nine.lua',
+	_LICENSE     = [[
+		The MIT License (MIT)
+
+		Copyright (c) 2015 Pablo Mayobre
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	]]
+}
+
 local calc = {}
 
 calc.cor = function (cor,x,y,w,h,i)
@@ -15,8 +44,6 @@ calc.bor = function (bor,x,y,w,h,i)
 
 	return x,y,sx,sy
 end
-
-local nine = {}
 
 function nine.draw(p,x,y,w,h,center,pad,img)
 	local img = img or p.default
@@ -118,48 +145,132 @@ function nine.process (patch)
 	}
 end
 
-function nine.convert(img)
+function nine.convert(img,name,encode)
+	local name = name or "image"
+	local encode = encode or false
 	local idata = img:getData()
 	local hor,ver = {},{}
+	local por,per = {},{}
+	local h,w = img:getHeight(),img:getWidth()
 
-	for i=0, img:getWidth() - 1 do
+	for i=0, w - 1 do
 		local r,g,b,a = idata:getPixel(i,0)
+		
 		if hor.x then
-			if r ~= 0 or g ~=0 or b ~= 0 or a ~= 255 then
+			if not hor.w and (r ~= 0 or g ~=0 or b ~= 0 or a ~= 255) then
 				hor.w = (i - 1) - hor.x
-				break
 			end
 		else
 			if r == 0 and g == 0 and b == 0 and a == 255 then
 				hor.x = i - 1
 			end
 		end
+		
+		local r,g,b,a = idata:getPixel(i,h - 1)
+		
+		if por.x then
+			if not por.w and (r ~= 0 or g ~= 0 or b ~= 0 or a ~= 255) then
+				por.w = (i - 1) - por.x
+			end
+		else
+			if r == 0 and g == 0 and b == 0 and a == 255 then
+				por.x = i - 1
+			end
+		end
+		
+		if hor.w and por.w then
+			break
+		end
 	end
 
-	for i=0, img:getHeight() - 1 do
+	if not hor.x then
+		error ("Inavalid 9-patch image, it doesnt contain an horizontal line",2)
+	end
+	if not hor.w then
+		hor.w = (w - 1) - hor.x
+	end
+
+	for i=0, h - 1 do
 		local r,g,b,a = idata:getPixel(0,i)
+		
 		if ver.y then
-			if r ~= 0 or g ~=0 or b ~= 0 or a ~= 255 then
+			if not ver.h and (r ~= 0 or g ~=0 or b ~= 0 or a ~= 255) then
 				ver.h = (i - 1) - ver.y
-				break
 			end
 		else
 			if r == 0 and g == 0 and b == 0 and a == 255 then
 				ver.y = i - 1
 			end
 		end
+		
+		local r,g,b,a = idata:getPixel(w - 1,i)
+		
+		if per.y then
+			if not per.h and (r ~= 0 or g ~= 0 or b ~= 0 or a ~= 255) then
+				per.h = (i - 1) - per.y
+			end
+		else
+			if r == 0 and g == 0 and b == 0 and a == 255 then
+				per.y = i - 1
+			end
+		end
+		
+		if ver.h and per.h then
+			break
+		end
+	end
+	
+	if not ver.y then
+		error("Inavalid 9-patch image, it doesnt contain a vertical line",2)
+	end
+	if not ver.h then
+		ver.h = (h - 1) - ver.y
+	end
+
+	por.w = por.x and (por.w and por.w or (w - 1 - por.x))or hor.w
+	per.h = per.y and (per.h and per.h or (h - 1 - per.y))or ver.h
+
+	por.x = por.x and por.x or hor.x
+	per.y = per.y and per.y or ver.y
+
+	local pad = {
+		ver.y - per.y,
+		(hor.x + hor.w) - (por.x + por.w)
+		(ver.y + ver.h) - (per.y + per.h),
+		hor.x - por.x,
+	}
+
+	local p = ""
+	
+	for i = 1, 4 do
+		p = p..pad[1]..","
 	end
 
 	local w,h = img:getWidth()-2, img:getHeight()-2
 	local asset = love.image.newImageData(w,h)
 	asset:paste(idata, 0, 0, 1, 1, w, h)
-	local str = [[local a = ...
-return {
-	image = a:gsub("%.","%/")..".png",
-	hor = {x = ]]..hor.x..",w = "..hor.w..[[},
-	ver = {y = ]]..ver.y..",h = "..ver.h..[[},
-	pad = {0,0,0,0},
-}]]
+
+	local str = [[return function (a)
+	return {
+		multiimage = false,
+		image = a.."/]]..name..[[.png",
+		hor = {x = ]]..hor.x..[[,w = ]]..hor.w..[[},
+		ver = {y = ]]..ver.y..[[,h = ]]..ver.h..[[},
+		pad = {]]..p..[[},
+	}
+end]]
+
+	if encode then
+		if type(encode) ~= "string" then
+			encode = ""
+		else
+			encode = encode.."/"
+		end
+
+		asset:encode(encode..name..".png")
+		love.filesystem.write(encode..name..".lua",str)
+	end
+
 	return str,asset
 end
 
