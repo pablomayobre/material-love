3f7e517a4527a658fd784aefe680bcfb27521f06
Pablo A. Mayobre <pablo.a.mayobre@gmail.com>
2016-10-10 07:53:35 -0300

    Finally changed all tabs to 4 spaces

--- libs/image.lua
+++ libs/image.lua
@@ -2,95 +2,95 @@ local image = {}
 
 local hsvShader
 
-local supported = select(2, love.getVersion()) == 10 or love.graphics.isSupported ("shader")
+local supported = select(2, love.getVersion()) == 10 or love.graphics.isSupported("shader")
 
 image.shader = [[
-	vec3 rgb2hsv(vec3 c)
-	{
-		vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-		vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-		vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-		float d = q.x - min(q.w, q.y);
-		float e = 1.0e-10;
-		return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-	}
+    vec3 rgb2hsv(vec3 c)
+    {
+        vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+        vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+        vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+        float d = q.x - min(q.w, q.y);
+        float e = 1.0e-10;
+        return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+    }
 
-	vec3 hsv2rgb(vec3 c)
-	{
-		vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-		vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-		return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-	}
+    vec3 hsv2rgb(vec3 c)
+    {
+        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    }
 
-	extern number sMod;
-	extern number vMod;
-	extern number opacity;
+    extern number sMod;
+    extern number vMod;
+    extern number opacity;
 
-	vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
-		vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
-		vec3 hsv = rgb2hsv(pixel.rgb);
-		hsv.r *= 1.0;
-		hsv.g *= sMod;
-		hsv.b *= vMod;
-		pixel = vec4(hsv2rgb(hsv).rgb, pixel.a * opacity);
-		return pixel * color;
-	}
+    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+        vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
+        vec3 hsv = rgb2hsv(pixel.rgb);
+        hsv.r *= 1.0;
+        hsv.g *= sMod;
+        hsv.b *= vMod;
+        pixel = vec4(hsv2rgb(hsv).rgb, pixel.a * opacity);
+        return pixel * color;
+    }
 ]]
 
 if supported then
-	hsvShader = love.graphics.newShader(image.shader)
+    hsvShader = love.graphics.newShader(image.shader)
 end
 
 -- Copied from github.com/EmmanuelOga/easing
 -- This function was contributed by Kikito
 local inOutQuad = function(t, b, c, d)
-	t = math.min(math.max(t, 0), 1)
-	t = t / d * 2
-	if t < 1 then
-		return c / 2 * t^2 + b
-	else
-		return -c / 2 * ((t - 1) * (t - 3) - 1) + b
-	end
+    t = math.min(math.max(t, 0), 1)
+    t = t / d * 2
+    if t < 1 then
+        return c / 2 * t^2 + b
+    else
+        return -c / 2 * ((t - 1) * (t - 3) - 1) + b
+    end
 end
 
 function image.new (img)
-	return {
-		image = img,
-		time = 0,
-		update = function (self,dt)
-			self.time = math.min(self.time + dt, 1)
-		end,
-		reset = function ()
-			self.time = 0
-		end,
-		draw = function (self, x, y, ra, sx, sy)
-			return image.draw(self.image, self.time, x, y, ra, sx, sy)
-		end,
-	}
+    return {
+        image = img,
+        time = 0,
+        update = function (self,dt)
+            self.time = math.min(self.time + dt, 1)
+        end,
+        reset = function ()
+            self.time = 0
+        end,
+        draw = function (self, x, y, ra, sx, sy)
+            return image.draw(self.image, self.time, x, y, ra, sx, sy)
+        end,
+    }
 end
 
 function image.draw(img, time, x, y, ra, sx, sy)
-	local r,g,b,a = love.graphics.getColor()
-	
-	local opacity = inOutQuad(time,  0, 1, 1)
+    local r,g,b,a = love.graphics.getColor()
 
-	if supported then
-		hsvShader:send("sMod", inOutQuad(time * 2, 0, 1, 1))
-		hsvShader:send("vMod", inOutQuad(time * 2, 0, 1, 1))
-		hsvShader:send("opacity", opacity)
+    local opacity = inOutQuad(time,  0, 1, 1)
 
-		love.graphics.setShader(hsvShader)
-	else --Fuck you Intel GMA 3100 combined with Windows!
-		love.graphics.setColor(r, g, b, a * opacity)
-	end
+    if supported then
+        hsvShader:send("sMod", inOutQuad(time * 2, 0, 1, 1))
+        hsvShader:send("vMod", inOutQuad(time * 2, 0, 1, 1))
+        hsvShader:send("opacity", opacity)
 
-	love.graphics.draw(img, x, y, ra, sx, sy)
+        love.graphics.setShader(hsvShader)
+    else
+        love.graphics.setColor(r, g, b, a * opacity)
+    end
 
-	if supported then
-		love.graphics.setShader()
-	else
-		love.graphics.setColor(r, g, b, a)
-	end
+    love.graphics.draw(img, x, y, ra, sx, sy)
+
+    if supported then
+        love.graphics.setShader()
+    else
+        love.graphics.setColor(r, g, b, a)
+    end
 end
 
 return image
--- libs/roundrect.lua
+++ libs/roundrect.lua
@@ -4,62 +4,62 @@
 local roundrect = {}
 
 roundrect.new = function (x, y, width, height, radius, precision)
-	local p = {} --Polygon!
-	--xs and ys are helper tables that determine the order of the corners (top-left -> clockwise)
-	--xs -> true = left, false = right
-	--ys -> true = top,  false = bottom
-	local xs, ys = {true,false,false,true}, {true,true,false,false}
+    local p = {} --Polygon!
+    --xs and ys are helper tables that determine the order of the corners (top-left -> clockwise)
+    --xs -> true = left, false = right
+    --ys -> true = top,  false = bottom
+    local xs, ys = {true,false,false,true}, {true,true,false,false}
 
-	if type(radius) ~= "table" then
-		radius = {radius or 0, radius or 0, radius or 0, radius or 0}
-	end
+    if type(radius) ~= "table" then
+        radius = {radius or 0, radius or 0, radius or 0, radius or 0}
+    end
 
-	for u = 1, 5 do
-		local i = ((u - 1) % 4) + 1
-		if u < 5 then
-			local j, size = i % 4 + 1, i % 2 == 1 and width or height
-			--Check if the radius is messed up (the sum of the radius is bigger than width/height)
-			--Correct it while keeping proportions
-			if radius[i] + radius[j] > size then
-				local porcentage = radius[i] / (radius[i] + radius[j])
-				radius[i], radius[j] = size * porcentage, size * (1 - porcentage)
-			end
-		end
+    for u = 1, 5 do
+        local i = ((u - 1) % 4) + 1
+        if u < 5 then
+            local j, size = i % 4 + 1, i % 2 == 1 and width or height
+            --Check if the radius is messed up (the sum of the radius is bigger than width/height)
+            --Correct it while keeping proportions
+            if radius[i] + radius[j] > size then
+                local porcentage = radius[i] / (radius[i] + radius[j])
+                radius[i], radius[j] = size * porcentage, size * (1 - porcentage)
+            end
+        end
 
-		--Add the points to the polygon
-		if u > 1 then
-			if radius[i] == 0 then
-				table.insert(p, xs[i] and x or x + width )
-				table.insert(p, ys[i] and y or y + height)
-			else
-				local precision = precision or radius[i]^2
-				for j = 0, precision do
-					local add = (xs[i] and ys[i]) and 2 or ((xs[i] and 3 or 2) + (ys[i] and 1 or 2))
-					print(add % 4)
-					local angle = (j / precision + add) * math.pi / 2
-					table.insert(p, (xs[i] and x + radius[i] or x + width  - radius[i]) + radius[i] * math.cos(angle))
-					table.insert(p, (ys[i] and y + radius[i] or y + height - radius[i]) + radius[i] * math.sin(angle))
-				end
-			end
-		end
-	end
+        --Add the points to the polygon
+        if u > 1 then
+            if radius[i] == 0 then
+                table.insert(p, xs[i] and x or x + width )
+                table.insert(p, ys[i] and y or y + height)
+            else
+                precision = precision or radius[i]^2
+                for j = 0, precision do
+                    local add = (xs[i] and ys[i]) and 2 or ((xs[i] and 3 or 2) + (ys[i] and 1 or 2))
+                    print(add % 4)
+                    local angle = (j / precision + add) * math.pi / 2
+                    table.insert(p, (xs[i] and x + radius[i] or x + width  - radius[i]) + radius[i] * math.cos(angle))
+                    table.insert(p, (ys[i] and y + radius[i] or y + height - radius[i]) + radius[i] * math.sin(angle))
+                end
+            end
+        end
+    end
 
-	return p
+    return p
 end
 
 roundrect.polygon = function (mode, polygon)
-	if mode == "both" then
-		love.graphics.polygon("fill", unpack(polygon))
-		love.graphics.polygon("line", unpack(polygon))
-	else
-		love.graphics.polygon(mode, unpack(polygon))
-	end
+    if mode == "both" then
+        love.graphics.polygon("fill", unpack(polygon))
+        love.graphics.polygon("line", unpack(polygon))
+    else
+        love.graphics.polygon(mode, unpack(polygon))
+    end
 end
 
 roundrect.draw = function (mode, ...)
-	local polygon = roundrect.new(...)
-	roundrect.polygon(mode, polygon)
-	return polygon
+    local polygon = roundrect.new(...)
+    roundrect.polygon(mode, polygon)
+    return polygon
 end
 
 return setmetatable(roundrect, { __call = function (s,...) return s.new(...) end })
